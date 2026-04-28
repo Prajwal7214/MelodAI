d4a2bae8f4e03b65c380ca92ef6f5a95f5220e4b
Prajwal7214 <prajwalshinde1320@gmail.com>
2026-04-28 13:43:36 +0530

    fix dotenv error

--- backend/melodai_project/mongo.py
+++ backend/melodai_project/mongo.py
@@ -1,8 +1,15 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://melodai_user:melodai123@cluster0.t1nfshd.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(MONGO_URI)
+db     = client["melodai_db"]
+
+music_history_collection = db["music_history"]
+favourites_collection    = db["favourites"]
 
 # MongoDB Atlas connection string
 MONGO_URI = os.getenv("MONGO_URI")
