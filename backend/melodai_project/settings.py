e55039a4c02c489d8277bb92480857458ee018ea
Prajwal7214 <prajwalshinde1320@gmail.com>
2026-05-02 12:44:23 +0530

    bug fixed

--- backend/melodai_project/settings.py
+++ backend/melodai_project/settings.py
@@ -72,5 +72,9 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
